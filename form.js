46ac3e69704f89b04883c3dfbfd7f530db0a9f7f
miguemulero <migue.mulero@gmail.com>
2026-03-09 12:45:46 +0100

    Update form.js

--- form.js
+++ form.js
@@ -16,27 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const ACCESS_TOKEN_STORAGE_KEY = "desiderativo_access_token";
 
   const BIBLIOGRAFIA_FILES = [
-    "files/sx8z883fd448", // ANaLISIS_DE_LAS_RESPUESTAS_AL_CUESTIONAR.pdf
-    "files/7w1gyfqjkp72", // bullying.pdf
-    "files/hrxrdmhp49hv", // CASO JADE.pdf
-    "files/0qgcgnydvcc8", // CASOS.pdf
-    "files/hwqelyy1l2ba", // CD DIANA.pdf
-    "files/8ngmft44byvr", // CD Graciela Celener.pdf
-    "files/1rsi50nq8clu", // CD pulsiones y defensas en patologías desvalimiento.pdf
-    "files/gl7ay2skq13f", // criterios de interpretación.pdf
-    "files/iv30eav88tni", // Cuadro proye - Catexias positivas y negativas.pdf
-    "files/u2mmmc39z9dg", // Cuestionario desiderativo aplicado a niños2.pdf
-    "files/iddnbkcevyww", // Cuestionario desiderativo-Sneiderman3.pdf
-    "files/fgqxehf8xx55", // Indicadores-Psicopatologicos - CD.pdf
-    "files/4abrxt47sqxg", // niños latentes.pdf
-    "files/iau8g20l0mdc", // Ocampo Arzeno - CD.pdf
-    "files/lknb4hybbw6a", // O_questionario_desiderativo_fundamentos.pdf
-    "files/rjl1r8pux2mm", // Preconsciente y su relación con el lenguaje.pdf
-    "files/swbkkonmnvip", // Psicodiagnostico Clinico 93-117.pdf
-    "files/gvh0x372su95", // Sneiderman_2011-Cuestionario.pdf
-    "files/7ce8utmwh0jq", // TEORÍA, TÉCNICA Y APLICACIÓN.pdf
-    "files/jsu5rk4g3gvw", // Una contribución a la interpretación del Cuestionario Desiderativo.pdf
-    "files/f12bscbw3ysu", // Vinculo hostil.pdf
+      "fileId": "files/wsd4dpqu0915",
+      "fileId": "files/jrdon4rz8pl9",
+      "fileId": "files/u8idv98iefwy",
+      "fileId": "files/l8147ymu4bv4",
+      "fileId": "files/yt6fwxwb8b22",
+      "fileId": "files/5faj6dpyrw46",
+      "fileId": "files/m8p1ukasexv2",
+      "fileId": "files/rvu2ta74ibd5",
+      "fileId": "files/8uvxi4aoos2f",
+      "fileId": "files/3yazxmiktsnk",
+      "fileId": "files/obvszki5yfvg",
+      "fileId": "files/octw6e79ydld",
+      "fileId": "files/q67v0mpvtzrn",
+      "fileId": "files/b1jkg2r87ru7",
+      "fileId": "files/1jh9xs4w2n50",
+      "fileId": "files/3it225iwkey2",
+      "fileId": "files/z5aru2ozop9k",
+      "fileId": "files/zm0iyz8zwldy",
+      "fileId": "files/3c216nwlmv3u",
+      "fileId": "files/ek8k1ef3d4h9",
+      "fileId": "files/kkwwdhpgzwjw",
   ];
 
   // ==========================================
